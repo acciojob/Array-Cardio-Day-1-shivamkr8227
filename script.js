e6f4bfcd88758d7f616f722001593ba828df36fc
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-05-05 13:07:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -29,14 +29,14 @@ const people = [
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's and return the filtered array
 export function myfilter() {
-
+return inventors.filter(inventor=>inventor.year>=1500 && inventor.year<1600);
 }
 
 // Array.prototype.map()
 // 2. Give us an array of the inventor first and last names (i.e. full name)
 // Ex: For the first inventor the full name will be 'Albert Einstein'
 export function map() {
-
+	return inventors.map(inventor => `${inventor.first} ${inventor.last}`);
 }
 
 
